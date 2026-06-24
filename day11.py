9d623266ccfb00195a0a17f5ac1e1e71f23214d2
Samikshya Mishra <248681724+mishraneera380-coder@users.noreply.github.com>
2026-06-24 15:20:38 +0545

    day11

---
+++ day11.py
@@ -0,0 +1,28 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater.")
+    else:
+        print("Second number is greater.")
+
+a=10
+b=20
+calculateGmean(a,b)
+isGreater(a,b) 
+
+def isGreater(a,v):
+    pass
+
+def isLesser(c,d):
+    if(c>d):
+        print("First number is smaller.")
+    else:
+        print("Second number is smaller.")
+
+c=12
+d=10
+calculateGmean(c,d)
+isLesser(a,b)
