ccbd44c55ec8e8eb31629e496148ae278b3dc8f4
Samikshya Mishra <samikshya812@gmail.com>
2026-09-06 19:26:49 +0545

    Update day2.py

---
+++ LearningPython/day2.py
@@ -0,0 +1,14 @@
+
+print("Samikshya") #string
+
+# string -> where a to z or A to Z character comes
+print(123) #number
+print(False) #boolean
+
+# Variables --> 
+bucket = 'water' #stirng
+cup = "coffee" #string
+age = 18 #integer
+isNepali = True #boolean
+
+magic-bucket = ["water", "coffee", "icecream"] #lists
