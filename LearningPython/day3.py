776c72318d70aec1394f6f2997c3ba501314a8c3
Samikshya Mishra <samikshya812@gmail.com>
2026-09-06 19:47:23 +0545

    Create day3.py

---
+++ LearningPython/day3.py
@@ -0,0 +1,14 @@
+
+name="Neera"
+name2="Samikshya"
+name3="Bhim"
+
+names= ["neera", "samikshya", "Bhim"] #list datatypes
+
+country="japan"
+country1="USA"
+country2="China"
+
+countries=["Japan", "USA", "China"] #list
+countries={"japan", "USA", "China"} #sets
+countries=("Japan", "USA", "China") #tuples
