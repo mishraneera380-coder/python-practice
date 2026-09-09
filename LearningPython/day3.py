1a99931573e8943d687d197d0dcd71cc18267d45
Samikshya Mishra <samikshya812@gmail.com>
2026-09-09 21:54:16 +0545

    Update day3.py

--- LearningPython/day3.py
+++ LearningPython/day3.py
@@ -11,14 +11,23 @@ country2="China"
 
 countries=["Japan", "USA", "China"] #list
 # print(countries[-1])
-countries.append("Nepal") #adds to last of list
-countries.pop()  #removes last item of list
-countries.insert(0, "Switzerland")  #adds to 0 index(first)
-print(countries)
+# countries.append("Nepal") #adds to last of list
+# countries.pop()  #removes last item of list
+# countries.insert(0, "Switzerland")  #adds to 0 index(first)
+# countries.remove("Japan")  #removes
+# countries[1] = 'London'
+
+# print(len(countries))
+# print(countries)
+
+countries=("Japan", "USA", "China") #tuples 
+print(countries[0])
+# countries[1] ="HOngKong"
+print(countries.count('China'))
+print(countries.index('USA'))
+
 
 countries={"japan", "USA", "China"} #sets
 # print(countries[1])
 
 
-countries=("Japan", "USA", "China") #tuples 
-# print(countries[1])
