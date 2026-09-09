2ab251f3eedca2669beca2b43688c5f9bfae74b0
Samikshya Mishra <samikshya812@gmail.com>
2026-09-09 21:47:28 +0545

    Update day3.py

--- LearningPython/day3.py
+++ LearningPython/day3.py
@@ -10,8 +10,15 @@ country1="USA"
 country2="China"
 
 countries=["Japan", "USA", "China"] #list
-print(countries[0])
+# print(countries[-1])
+countries.append("Nepal") #adds to last of list
+countries.pop()  #removes last item of list
+countries.insert(0, "Switzerland")  #adds to 0 index(first)
+print(countries)
+
 countries={"japan", "USA", "China"} #sets
+# print(countries[1])
+
 
 countries=("Japan", "USA", "China") #tuples 
-
+# print(countries[1])
