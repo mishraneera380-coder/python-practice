7c6c0c4529ee6234d6cfd1cea68d7bd4e0586b6e
Samikshya Mishra <samikshya812@gmail.com>
2026-09-17 19:28:06 +0545

    Update Assignment.py

--- LearningPython/Assignment.py
+++ LearningPython/Assignment.py
@@ -11,7 +11,7 @@ print(courses)
 # 3. Use a for loop to display all available courses.
 
 for course in courses:
-    if "Python with AI/ML" in course and "JavaScript" in course and  "Flutter" in course and  "MERN Stack" in course :
+    if "Python with AI/ML" in course and "JavaScript" in course and  "Flutter" in course and  "MERN Stack" in course:
         print("All available courses", course)
 else:
     print("Not all courses", course)
