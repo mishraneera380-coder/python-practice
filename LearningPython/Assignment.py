0f9ef1b20ee7c4507f8d00cc0549565ed1e4fcdb
Samikshya Mishra <samikshya812@gmail.com>
2026-09-17 19:36:11 +0545

    Update Assignment.py

--- LearningPython/Assignment.py
+++ LearningPython/Assignment.py
@@ -5,13 +5,19 @@ print("Welcome to DPLMS Student Regstration System.")
 # 2. create a list containing these courses: Python with AI/ML, JavaScript, Flutter and MERN Stack.
 
 courses = ["python with AI/ML", "JavaScript", "Flutter", "MERN Stack"]
-
+required = ["python with AI/ML", "JavaScript", "Flutter", "MERN Stack"]
+found =[]
 print(courses)
 
 # 3. Use a for loop to display all available courses.
 
-for course in courses:
-    if "Python with AI/ML" in course and "JavaScript" in course and  "Flutter" in course and  "MERN Stack" in course:
-        print("All available courses", course)
+for req in required:
+    if req in courses:
+        found.append(req)
+    else:
+        print("Missing course.", req)
+
+if len(found) == len(required):
+    print("All available courses.", found)
 else:
-    print("Not all courses", course)
+    print("Not all courses are available.")
